f595f17a1f6d15cfcda550003b86d53619411406
Njong <njongemy@gmail.com>
2023-01-13 21:21:26 +0100

    feat: add RSS fees

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,9 @@
+import rss from '@astrojs/rss';
+
+export const get = () => rss({
+  title: 'Blog',
+  description: 'My new blog made with astro',
+  site: 'https://blog-astro-gamma.vercel.app/',
+  items: import.meta.glob('./**/*.md'),
+  customData: `<language>en-us</language>`,
+});
