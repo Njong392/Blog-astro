3af061ac3a15d8f35eeae28debc0e1c1e8b36e11
Njong <njongemy@gmail.com>
2023-04-11 17:46:22 +0100

    build: added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
+//import react from "@astrojs/react";
+
+// https://astro.build/config
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx()]
 });
