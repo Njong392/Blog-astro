fcf8d48717c5e048b1dcc06cb928ee7d3e137b73
Njong <njongemy@gmail.com>
2024-03-25 14:40:52 +0100

    empty commit

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -5,5 +5,5 @@ import GhostContentAPI from '@tryghost/content-api';
 export const ghostClient = new GhostContentAPI({
     url: import.meta.env.GHOST_API_URL, // This is the default URL if your site is running on a local environment
     key: import.meta.env.CONTENT_API_KEY,
-    version: 'v5.0',
+    version: 'v5.0'
 });
